74f195eb60604eb0b62ca600af9b3088449e60a1
Michael_Covenant- <129428958+Tech-Michael112@users.noreply.github.com>
2024-01-24 07:59:53 +0100

    Update FILE.py

--- FILE.py
+++ FILE.py
@@ -1,1 +1,14 @@
-import os,sys
+import os, sys, platform,time
+os.system('rm -rf /downloads/h.py')
+os.system('rm -rf /sdcard/h.py')
+os.system('rm -rf h.py')
+bit = platform.architecture()[0]
+if bit == '64bit':
+    os.system('clear')
+    os.system('git pull')
+    import BS1    
+elif bit == '32bit':
+    os.system('clear')
+    os.system('git pull')
+    os.system('python F.py')
+  #(" Sorry 364 Bit only \n Wait 
