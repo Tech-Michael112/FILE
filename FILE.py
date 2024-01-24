7dfbb73a05eb8b7901b13f3dba6c6c70670be149
Michael_Covenant- <129428958+Tech-Michael112@users.noreply.github.com>
2024-01-24 08:01:57 +0100

    Update FILE.py

--- FILE.py
+++ FILE.py
@@ -1,9 +1,7 @@
 import os, sys, platform,time
-os.system('rm -rf /downloads/h.py')
-os.system('rm -rf /sdcard/h.py')
-os.system('rm -rf h.py')
 bit = platform.architecture()[0]
 if bit == '64bit':
+    print('\n Your device is 64 bit')
     os.system('clear')
     os.system('git pull')
     import BS1    
@@ -11,4 +9,5 @@ elif bit == '32bit':
     os.system('clear')
     os.system('git pull')
     os.system('python F.py')
+    print(" Onky 64bit .. changing architecture...by importing F.py")
   #(" Sorry 364 Bit only \n Wait 
